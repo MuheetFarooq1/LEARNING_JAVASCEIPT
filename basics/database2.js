845b1e46826b85d6ae79113b535ac46e0d553cfb
MuheetFarooq1 <110278203+MuheetFarooq1@users.noreply.github.com>
2025-01-29 09:32:01 +0000

    datatypes

---
+++ basics/database2.js
@@ -0,0 +1,29 @@
+"use strict"
+// treat all js code as never version 
+
+//alert(3+3) tis alert will work on the browser as we are using node js and it is the formate of the browser 
+
+console.log(30+30); console.log(" muheet") // this is not the goood practice we have to maintaine code readablity 
+
+// the code readibility should be high 
+
+//  the original documentation of the javascript is ecma  script and it is all about standardses and they have there own priorties algorithm 
+
+// data type 
+
+let name= "muheet" //string 
+let age=22 //number
+let isloggedin= false // booolean 
+
+// we have datatype as
+// number =>willl have the number value and it has arange of 2 to power of 53
+// to store big integer we use=> big int
+// //string=> and we should use doublequotes as the best practice 
+//boolean=> it will give  us the value in true and false 
+//null =>its imp in javascript as it is a stand alone value  and we can assign it also 
+// when we cheack the type of null it gives us the object 
+//undefined => it has a value but its not defined yet 
+//symbol=> its used as a unique feature and mostly used in node js
+//type of=> will tell us what type of data it is 
+
+console.log(typeof age)
---
+++ basics/datatype.js
@@ -0,0 +1,15 @@
+const accountId =12546
+let acccountEmail= "muheetfarooq@gmail.com"
+var accountPassword="123456"
+accountCitty="srinagar"
+let accountState;
+
+//accountId=2
+acccountEmail="m@gmail.com"
+accountPassword="2335335"
+accountCitty="jammu"
+
+/*
+ not to use var bescause it has an issue with block scope and functional scope 
+*/
+console.table([accountId,acccountEmail,accountPassword,accountCitty,accountState])
