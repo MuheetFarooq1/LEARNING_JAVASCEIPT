8da89f1f8ec143bb92ee69b10fb5bfa12d7e033b
MuheetFarooq1 <110278203+MuheetFarooq1@users.noreply.github.com>
2025-01-29 09:44:45 +0000

    conversion of data types

---
+++ basics/conversion.js
@@ -0,0 +1,26 @@
+let score =33
+let value ="33"
+//console.log(typeof (score))
+//console.log(typeof (value))
+let change =Number(value)
+//console.log(typeof(change));
+//console.log(change);
+
+// "33"=> can be changed into 33
+//"33ab"=> it will give us NaN
+//"true"= 1
+//"false"=0
+
+let isloggedin =1
+let boooleanchange = Boolean(isloggedin)
+//console.log(boooleanchange);
+
+//""=>false
+//"muheet"=> true 
+
+let somenumber=33
+let stringnumber=String(somenumber)
+console.log(typeof stringnumber);
+
+console.log(stringnumber)
+;
