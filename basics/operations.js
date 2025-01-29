81a670704b6447e6abd84f1be119a2dfd462c662
MuheetFarooq1 <110278203+MuheetFarooq1@users.noreply.github.com>
2025-01-29 10:18:07 +0000

    comparision and operations

---
+++ basics/operations.js
@@ -0,0 +1,43 @@
+//*************************operations******************** */
+
+let value =3
+let negvalue= -value
+//console.log(negvalue);
+  
+//console.log(2+12);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+
+
+
+let str1="muheet"
+let str2="farooq"
+ let str3 = str1+str2
+//  console.log(str3);
+
+
+//  console.log("1"+2);
+//  console.log(1+"2");
+//  console.log("1"+2+2);
+//  console.log(1+2+"2");
+//  console.log(3+3*5%3);
+ 
+//  console.log(+true)
+//  console.log(+"")
+
+
+ let num1,num2,num3
+ num1=num2=num3=2+2
+
+
+ let gamecounter =100
+ gamecounter++
+ ++gamecounter
+ console.log(gamecounter);
+ 
+ 
+ 
+
+
+
